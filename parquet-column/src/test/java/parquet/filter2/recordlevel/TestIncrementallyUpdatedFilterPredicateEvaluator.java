4bb3681abb96987fa1058286bd110656921dd399
Prakash Das <pdas@informatica.com>
2015-07-14 16:05:27 -0700

    Adds a test case that mirrors how a ValueInspector object is used in batched reading mode of execution.

--- parquet-column/src/test/java/parquet/filter2/recordlevel/TestIncrementallyUpdatedFilterPredicateEvaluator.java
+++ parquet-column/src/test/java/parquet/filter2/recordlevel/TestIncrementallyUpdatedFilterPredicateEvaluator.java
@@ -136,6 +136,57 @@ public class TestIncrementallyUpdatedFilterPredicateEvaluator {
     assertTrue(evaluate(intIsNull));
   }
 
+  @Test
+  public void testBatchModeValueInspector() {
+    // known, and set to false criteria, null considered false
+    ValueInspector v = intIsEven();
+    v.resetBatch(); // start of batch operation
+    for (int i = 11; i < 20; ++i) {
+      v.update(i);
+      v.prepareToSetNextResult();
+    }
+    // known and set to null, null considered false
+    v.updateNull();
+    v.prepareToSetNextResult();
+   
+    // retrieve results that were set earlier
+    for (int i = 11; i < 20; ++i) {
+      if (i % 2 == 0)
+        assertTrue(evaluate(v));
+      else
+        assertFalse(evaluate(v));
+      v.reset();
+    }
+    assertFalse(evaluate(v));
+    v.reset();
+
+    // unknown, null considered false
+    // v.resetBatch(); is not necessary all the results have been retrieved.
+    assertFalse(evaluate(v));
+
+    // known, and set to false criteria, null considered true
+    ValueInspector intIsNull = intIsNull();
+    intIsNull.resetBatch(); // start of batch operation
+    intIsNull.update(10); // 1st result
+    intIsNull.prepareToSetNextResult();
+    // known, and set to false criteria, null considered true
+    intIsNull.updateNull(); // 2nd result
+    intIsNull.prepareToSetNextResult();
+    intIsNull.updateNull(); // 3rd result
+    intIsNull.prepareToSetNextResult();
+   
+    // retrieve results 1st & 2nd
+    assertFalse(evaluate(intIsNull));
+    intIsNull.reset();
+    assertTrue(evaluate(intIsNull));
+    intIsNull.reset();
+    
+    // unknown, null considered true
+    // resetBatch() is necessary because the 3rd result set above wasn't retrieved
+    intIsNull.resetBatch();
+    assertTrue(evaluate(intIsNull));
+  }
+
   private void doOrTest(ValueInspector v1, ValueInspector v2, int v1Value, int v2Value, boolean expected) {
     v1.update(v1Value);
     v2.update(v2Value);
